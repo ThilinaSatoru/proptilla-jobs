edcf12c9bfb4928fa395b1faa842d181a5bdc828
Thilina Satoru <satoru.thilina@gmail.com>
2023-09-28 12:33:21 +0530

    crud done.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
     ],
 
     daisyui: {
-        themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: "light", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
